00a77c131ec612ff23c354c2a0e38a2ba90d0c20
jamesfowler-dev <james_fowler21@hotmail.com>
2025-11-13 15:00:01 +0000

    edit models for debugging exercise

--- blog/models.py
+++ blog/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
 
-    class Meta:
+    class Meta: 
         ordering = ["-created_on"]
     
     def __str__(self):
